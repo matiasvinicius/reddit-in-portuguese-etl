56e53685267cff553f6ebd85426e111fa850e52c
Vinicius Matias <vinicius.amatias@gmail.com>
2022-09-05 21:57:50 -0300

    feat: subreddit name in status page

--- reddit_etl/extraction/lib/status_utils.py
+++ reddit_etl/extraction/lib/status_utils.py
@@ -5,4 +5,5 @@ def get_last_submissions(n):
     last_submissions = models.Submission.objects.order_by('-extraction_date')[:10]
     for i, submission in enumerate(last_submissions):
         last_submissions[i].author_name = submission.author.username
+        last_submissions[i].subreddit_name = submission.subreddit.name
     return last_submissions
