093109ee5cbcaf3274fe14ec72aed7f371352c31
Vinicius Matias <vinicius.amatias@gmail.com>
2022-09-05 21:57:24 -0300

    temp: try except in populate_db main loop

--- reddit_etl/extraction/management/commands/populate_db.py
+++ reddit_etl/extraction/management/commands/populate_db.py
@@ -75,91 +75,16 @@ class Command(BaseCommand):
                 result = requests.get("https://api.pushshift.io/reddit/submission/search", params=payload)
 
                 for submission in result.json()["data"]:         
-                    author_praw = reddit.redditor(submission["author"])
-                    submission_praw = reddit.submission(submission["id"])    
-
                     try: 
-                        author_removed = False
-                        author_praw.id is None
-                    except:
-                        author_removed = True
+                        author_praw = reddit.redditor(submission["author"])
+                        submission_praw = reddit.submission(submission["id"])    
 
-                    if author_removed:
-                        Author.objects.update_or_create(
-                            id ='removed',
-                            username = None,
-                            created_utc = datetime(1970,1,1, tzinfo=timezone.utc),
-                            defaults={
-                                "has_verified_email": False,
-                                "karma": 0,
-                                "extraction_date": datetime.now(tz=timezone.utc)
-                            }
-                        )
-                        author_id = 'removed'
-
-                    else:
-                        Author.objects.update_or_create(
-                            id = author_praw.id,
-                            username = author_praw.name,
-                            created_utc = datetime.fromtimestamp(author_praw.created_utc, tz=timezone.utc),
-                            defaults={
-                                "has_verified_email": author_praw.has_verified_email,
-                                "karma": author_praw.link_karma,
-                                "extraction_date": datetime.now(tz=timezone.utc)
-                            }
-                        )
-                        author_id = author_praw.id
-
-
-                    Submission.objects.update_or_create(
-                        id = submission_praw.id,
-                        author = Author.objects.get(id=author_id),
-                        subreddit = Subreddit.objects.get(id=submission["subreddit_id"]),
-                        created_utc = datetime.fromtimestamp(submission_praw.created_utc, tz=timezone.utc),
-                        defaults={
-                            "name": submission_praw.name,
-                            "title": submission_praw.title,
-                            "selftext": submission_praw.selftext,
-                            "distinguished": submission_praw.distinguished,
-                            "spoiler": submission_praw.spoiler,
-                            "allow_live_comments": submission["allow_live_comments"],
-                            "is_video": submission["is_video"],
-                            "media_only": submission["media_only"],
-                            "is_self": submission_praw.is_self,
-                            "is_original_content": submission_praw.is_original_content,
-                            "link_flair_text": submission_praw.link_flair_text,
-                            "num_comments": submission_praw.num_comments,
-                            "over_18": submission_praw.over_18,
-                            "permalink": submission_praw.permalink,
-                            "score": submission_praw.score,
-                            "url": submission_praw.url,
-                            "edited": datetime.fromtimestamp(submission_praw.edited, tz=timezone.utc),
-                            "upvote_ratio": submission_praw.upvote_ratio,
-                            "extraction_date": datetime.now(tz=timezone.utc)
-                        }
-                    )
-
-                    for tag in submission["treatment_tags"]:
-                        TreatmentTags.objects.create(
-                            title = tag,
-                            submission = Submission.objects.get(id=submission["id"]),
-                            subreddit = Subreddit.objects.get(id=submission["subreddit_id"])
-                        )
-
-                    for comment in submission_praw.comments:
-                        comment_praw = reddit.comment(comment.id)
-                        
-                        if comment_praw.author is None:
-                            author_removed = True
-                        else:
+                        try: 
                             author_removed = False
-
-                        try:
-                            author_comment_praw = reddit.redditor(comment_praw.author)
-                            author_comment_praw.id
+                            author_praw.id is None
                         except:
                             author_removed = True
-                      
+
                         if author_removed:
                             Author.objects.update_or_create(
                                 id ='removed',
@@ -170,38 +95,116 @@ class Command(BaseCommand):
                                     "karma": 0,
                                     "extraction_date": datetime.now(tz=timezone.utc)
                                 }
-                            )   
+                            )
                             author_id = 'removed'
 
                         else:
                             Author.objects.update_or_create(
-                                id = author_comment_praw.id,
-                                username = author_comment_praw.name,
-                                created_utc = datetime.fromtimestamp(author_comment_praw.created_utc, tz=timezone.utc),
+                                id = author_praw.id,
+                                username = author_praw.name,
+                                created_utc = datetime.fromtimestamp(author_praw.created_utc, tz=timezone.utc),
                                 defaults={
-                                    "has_verified_email": author_comment_praw.has_verified_email,
-                                    "karma": author_comment_praw.link_karma,
+                                    "has_verified_email": author_praw.has_verified_email,
+                                    "karma": author_praw.link_karma,
                                     "extraction_date": datetime.now(tz=timezone.utc)
                                 }
                             )
-                            author_id = author_comment_praw.id
+                            author_id = author_praw.id
 
-                        Comment.objects.update_or_create(
-                            id = comment_praw.id,
+
+                        Submission.objects.update_or_create(
+                            id = submission_praw.id,
                             author = Author.objects.get(id=author_id),
                             subreddit = Subreddit.objects.get(id=submission["subreddit_id"]),
-                            submission = Submission.objects.get(id=submission["id"]),
-                            created_utc = datetime.fromtimestamp(comment_praw.created_utc, tz=timezone.utc),
+                            created_utc = datetime.fromtimestamp(submission_praw.created_utc, tz=timezone.utc),
                             defaults={
-                                "parent_id": comment_praw.parent_id,
-                                "body": comment_praw.body,
-                                "distinguished": comment_praw.distinguished,
-                                "is_submitter": comment_praw.is_submitter,
-                                "score": comment_praw.score,
-                                "permalink": comment_praw.permalink,
+                                "name": submission_praw.name,
+                                "title": submission_praw.title,
+                                "selftext": submission_praw.selftext,
+                                "distinguished": submission_praw.distinguished,
+                                "spoiler": submission_praw.spoiler,
+                                "allow_live_comments": submission["allow_live_comments"],
+                                "is_video": submission["is_video"],
+                                "media_only": submission["media_only"],
+                                "is_self": submission_praw.is_self,
+                                "is_original_content": submission_praw.is_original_content,
+                                "link_flair_text": submission_praw.link_flair_text,
+                                "num_comments": submission_praw.num_comments,
+                                "over_18": submission_praw.over_18,
+                                "permalink": submission_praw.permalink,
+                                "score": submission_praw.score,
+                                "url": submission_praw.url,
+                                "edited": datetime.fromtimestamp(submission_praw.edited, tz=timezone.utc),
+                                "upvote_ratio": submission_praw.upvote_ratio,
                                 "extraction_date": datetime.now(tz=timezone.utc)
-                            }  
+                            }
                         )
+
+                        for tag in submission["treatment_tags"]:
+                            TreatmentTags.objects.create(
+                                title = tag,
+                                submission = Submission.objects.get(id=submission["id"]),
+                                subreddit = Subreddit.objects.get(id=submission["subreddit_id"])
+                            )
+
+                        for comment in submission_praw.comments:
+                            comment_praw = reddit.comment(comment.id)
+                            
+                            if comment_praw.author is None:
+                                author_removed = True
+                            else:
+                                author_removed = False
+
+                            try:
+                                author_comment_praw = reddit.redditor(comment_praw.author)
+                                author_comment_praw.id
+                            except:
+                                author_removed = True
+                        
+                            if author_removed:
+                                Author.objects.update_or_create(
+                                    id ='removed',
+                                    username = None,
+                                    created_utc = datetime(1970,1,1, tzinfo=timezone.utc),
+                                    defaults={
+                                        "has_verified_email": False,
+                                        "karma": 0,
+                                        "extraction_date": datetime.now(tz=timezone.utc)
+                                    }
+                                )   
+                                author_id = 'removed'
+
+                            else:
+                                Author.objects.update_or_create(
+                                    id = author_comment_praw.id,
+                                    username = author_comment_praw.name,
+                                    created_utc = datetime.fromtimestamp(author_comment_praw.created_utc, tz=timezone.utc),
+                                    defaults={
+                                        "has_verified_email": author_comment_praw.has_verified_email,
+                                        "karma": author_comment_praw.link_karma,
+                                        "extraction_date": datetime.now(tz=timezone.utc)
+                                    }
+                                )
+                                author_id = author_comment_praw.id
+
+                            Comment.objects.update_or_create(
+                                id = comment_praw.id,
+                                author = Author.objects.get(id=author_id),
+                                subreddit = Subreddit.objects.get(id=submission["subreddit_id"]),
+                                submission = Submission.objects.get(id=submission["id"]),
+                                created_utc = datetime.fromtimestamp(comment_praw.created_utc, tz=timezone.utc),
+                                defaults={
+                                    "parent_id": comment_praw.parent_id,
+                                    "body": comment_praw.body,
+                                    "distinguished": comment_praw.distinguished,
+                                    "is_submitter": comment_praw.is_submitter,
+                                    "score": comment_praw.score,
+                                    "permalink": comment_praw.permalink,
+                                    "extraction_date": datetime.now(tz=timezone.utc)
+                                }  
+                            )
+                    except:
+                        continue
 
                 after = int(submission_praw.created_utc)
 
